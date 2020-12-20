3d37e5f8eeefcccf97c35631f15888c6c87b21a2
Shunsuke Kawabata <shun.92@gmail.com>
2020-12-20 18:16:10 +0900

    ファイルなくても利用できるように

--- protocols.go
+++ protocols.go
@@ -2,22 +2,27 @@ package main
 
 import (
 	"errors"
-	"io/ioutil"
 	"strings"
-
-	"github.com/BurntSushi/toml"
 )
+
+// go:embed protocol.toml
+// var protocolsFile []byte
 
 type protocol struct {
 	port map[string]interface{}
 }
 
 func (p *protocol) new() {
-	bytes, err := ioutil.ReadFile("./protocol.toml")
-	if err != nil {
-		panic(err)
-	}
-	toml.Unmarshal(bytes, &p.port)
+	p.port["ssh"] = 22
+	p.port["http"] = 80
+	p.port["https"] = 443
+	p.port["rdp"] = 3389
+	p.port["kibana"] = 5601
+	p.port["ne4j"] = 7474
+	p.port["bolt"] = 7687
+	p.port["elasticsearch"] = 9200
+	p.port["scaler"] = 30600
+	// toml.Unmarshal(protocolsFile, &p.port)
 }
 
 func (p *protocol) getPortNo(protocolName interface{}) (int, error) {
